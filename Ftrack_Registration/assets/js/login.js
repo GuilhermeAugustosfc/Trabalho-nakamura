28b0622e07420840bbd39f827dc00798993efb24
Lucas Consa <lucaslssouza2015@gmail.com>
2020-06-25 19:51:29 -0300

    subindo o ftragistration

---
+++ Ftrack_Registration/assets/js/login.js
@@ -0,0 +1,36 @@
+var base_url = window.location.origin;
+$(document).ready(function () {
+    $("#btn-login").on("click",function() { 
+        
+        var email = $("#email").val()
+        var senha =  $("#senha").val()
+
+        var enviarDados = {
+            email: email,
+            senha: senha
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "index.php/Login/autenticar/",
+            data: enviarDados,
+            dataType: "JSON",
+            success: function (data) {
+                if (data.status) {
+                    window.location.href = "Dashboard";
+                }else{
+                    $("#senha").val("");        
+                    iziToast.error({
+                        title: 'ATENÇÃO!!',
+                        message: 'Usuario ou senha inválidos',
+                    });
+                }
+            }
+        });
+     })    
+});
+
+
+$(document).keypress(function(e) {
+    if(e.which == 13) $('#btn-login').click();
+});
